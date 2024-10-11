61476d42ffd6126d3b3845434462212ed632d967
zzz <zzz@zzzdeMacBook-Pro.local>
2024-10-11 15:56:10 +0800

    fix error

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 # mused
+=======
+# mused_1
+## paper
+## run eval with openAI keys
+
+## run eval with hugging-face Model
+
+>>>>>>> 47f44f4 (fix error)
--- script/eval_hf_model.sh
+++ script/eval_hf_model.sh
@@ -1,11 +1,11 @@
 
 LOGITS_INPUT_PATH="../data/test/test_prompt.jsonl"
-
 BASE_RES_PATH="../eval_res"
 
-NAME="llama8B_instruct_res_eval_sets"
-OUTPUT_PATH="${BASE_RES_PATH}/${NAME}"
+NAME="MODEL_NAME" 
 model_path="MODEL_PATH"
+
+OUTPUT_PATH="${BASE_RES_PATH}/${NAME}"
 
 mkdir -p $OUTPUT_PATH
 python ../src/infer_codes/llama3_8b/uf_sample_batch.py --input-path $LOGITS_INPUT_PATH --output-path "${OUTPUT_PATH}/final_prompts.jsonl" --model-path $model_path
