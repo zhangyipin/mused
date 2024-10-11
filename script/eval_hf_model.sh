3f08ae38cc9b5fb248de3a255094fb09a200e5ea
zzz <zzz@zzzdeMacBook-Pro.local>
2024-10-11 15:59:45 +0800

    fix

--- script/eval_hf_model.sh
+++ script/eval_hf_model.sh
@@ -8,7 +8,7 @@ model_path="MODEL_PATH"
 OUTPUT_PATH="${BASE_RES_PATH}/${NAME}"
 
 mkdir -p $OUTPUT_PATH
-python ../src/infer_codes/llama3_8b/uf_sample_batch.py --input-path $LOGITS_INPUT_PATH --output-path "${OUTPUT_PATH}/final_prompts.jsonl" --model-path $model_path
+python ../src/infer_codes/hf_model/uf_sample_batch.py --input-path $LOGITS_INPUT_PATH --output-path "${OUTPUT_PATH}/final_prompts.jsonl" --model-path $model_path
 res_path="${OUTPUT_PATH}/final_prompts.jsonl"
 python ../src/codes/scores.py ${res_path} ${res_path}.score_res ${res_path}.report_res ${res_path}.report_res.xlsx
 
